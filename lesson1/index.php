d353109d5e66686e889ea307e399a813f4ecd078
Umid2720 <umidbekruzmetov309@gmail.com>
2026-02-27 17:51:16 +0500

    docs: learn PHP functions, type hinting and strict_types usage

--- lesson1/index.php
+++ lesson1/index.php
@@ -24,6 +24,7 @@ $number--;
 print $number;
 print "\n";
 
+// O’zgaruvchan turi avtomatik tushuniladi:
 $foo = "Salom, O'zbekiston!";      // string
 $number = 42;                      // integer
 $pi = 3.14;                       // float
@@ -35,3 +36,25 @@ $person = [                       // associative array
     "age" => 30,
     "city" => "Tashkent"
 ];  
+
+// PHP’da funksiya yaratilayotganda, uning nomi oldidan function kalit so’zi qo’yiladi:
+function greet($name) {
+    return "Salom, $name!";
+}
+
+// Funksiyani chaqirish:
+echo greet("Umidbek"); // Natija: Salom, Umidbek!
+
+function foo (int $number1, int $number2): void
+{
+	print $number1 + $number2;
+}
+
+foo(10, 15); // 25
+
+// PHP dinamik turlovchi dasturlash tili bo’lganligi uchun, funksiyaning parametri turiga int ko’rsatib, funksiyani chaqirayotganda, unga argument sifatida yuborgan qiymatni string turida bersak ham xato bo’lmaydi. PHP’ning o’zi argumentning ma’lumot turini avtomatik int’ga o’tkazadi:
+
+echo "<hr/>";
+foo("10", "15"); // 25
+
+// PHP’ni statik turlovchi dasturlash tiliga o’tkazib ishlatish maslahat beriladi. Buning uchun quyidagi kod har bir php faylining yuqori qismida bo’lishi shart: declare(strict_types=1);
