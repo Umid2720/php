c42041c563ccd2f19c6b1a5d47fad45d0b0befea
Umid2720 <umidbekruzmetov309@gmail.com>
2026-02-27 17:22:00 +0500

    docs: add PHP CLI basics and hello world

---
+++ lesson1/index.php
@@ -0,0 +1,10 @@
+<!-- 
+ php -v     php o'rnatilganligini tekshirish 
+ php -a     interaktiv rejimda ishlash uchun
+ ctrl c || ctrl d || exit    interaktiv rejimdan chiqish uchun
+ php -S localhost:8505    vaqtinchalik web serverni yaratish
+ ctrl c    web serverni to'xtatish uchun
+
+-->
+
+print "Hello, World!";
