6620ff46ad50090528ebbcbfa15ef56d5d31f899
Umid2720 <umidbekruzmetov309@gmail.com>
2026-03-01 23:44:47 +0500

    docs: learn PHP GET requests and form handling basics

--- lesson4/index.php
+++ lesson4/index.php
@@ -4,6 +4,9 @@ ini_set('display_errors', '1');
 error_reporting(E_ALL);
 
 // belgi orqali PHP’ni to’xtatish mumkin va undan keyin oddiy HTML kod yozish mumkin. Hozirgi misolda PHP’ni yopib, undan keyin HTML qatorlarini ishlatib ko’ramiz:
+
+// Php’da $_GET super global ko’pligi bor. Bu ko’plikda GET so’rovlari joylashgan bo’ladi:
+print_r($_GET);
 ?>
 
 <!DOCTYPE html>
@@ -11,14 +14,16 @@ error_reporting(E_ALL);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lesson 4 | PHP</title>
+    <title>Lesson 4 | PHP and HTML</title>
 </head>
 <body>
 
     <h1>Assalomu alaykum!!!</h1>
 
     <form>
-        Ismingiz: <input type="text">
+        Ismingiz: <input type="text" name="givenName"> <br />
+        Familiyangiz: <input type="text" name="familyName"> <br />
+        <button type="submit">Yuborish</button>
     </form>
     
 </body>
