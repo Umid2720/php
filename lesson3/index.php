697c4322db6f223c6139457a5f8f653279a07508
Umid2720 <umidbekruzmetov309@gmail.com>
2026-03-01 16:58:26 +0500

    docs: learn PHP error types and warnings overview

--- lesson3/index.php
+++ lesson3/index.php
@@ -46,6 +46,24 @@ print('<hr>');
 $result = isset($foo['0'], $foo['1087']);
 var_dump($result);
 
+// Shu o’rinda, PHP’da funksiyalar juda ham ko’p, shuning uchun ham dasturchining eng zo’r qobiliyatlaridan biri - bu to’g’ri va kerakli ma’lumotni topa olish.
+
+/*
+    Xato turlari:
+    Depreciated (eskirgan)
+    Biror ishlatgan kodimiz, misol uchun funksiyamiz - eskirgan va PHP’ning keyingi versiyalarida ishlamaydi, -  degan ma’nodagi ogohlantirish.
+
+    Notice (e’tibor bering)
+    Kichik xato. PHP ishlashda davom etadi. Lekin, kodimizda ular ham bo’lmasligi kerak.
+
+    Warning (ogohlantirish)
+    Jiddiy xato. Lekin, PHP ishlashda davom etadi.
+    
+    Fatal (mash’um xato)
+    Juda katta xato. Bu vaziyatda PHP ishlashni davom ettira olmaydi.
+    
+    Ushbu turdagi xatolar yozayotgan kodimizda bo’lmasligi shart.
+*/
 
 
 
