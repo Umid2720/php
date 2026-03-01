ab456201b55762fb1c8e70f23cf8621ba9d0b473
Umid2720 <umidbekruzmetov309@gmail.com>
2026-03-01 17:10:40 +0500

    docs: learn mixing PHP and HTML in a single file

---
+++ lesson4/index.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+// belgi orqali PHP’ni to’xtatish mumkin va undan keyin oddiy HTML kod yozish mumkin. Hozirgi misolda PHP’ni yopib, undan keyin HTML qatorlarini ishlatib ko’ramiz:
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lesson 4 | PHP</title>
+</head>
+<body>
+
+    <h1>Assalomu alaykum!!!</h1>
+    
+</body>
+</html>
